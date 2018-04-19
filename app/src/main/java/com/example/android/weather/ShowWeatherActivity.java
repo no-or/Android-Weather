8ee64e5b798cdc9e17cda92224446ca4796aa599
Noor Khan <noor_khan@alumni.ubc.ca>
2018-04-19 01:57:01 -0700

    2 activity Layouts are completed

---
+++ app/src/main/java/com/example/android/weather/ShowWeatherActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_weather);
+    }
+}
